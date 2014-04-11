402035e65fb38b672a85c66b0baddf2dec827b69
PsMonkey <PsMonkey@dontcareabout.us>
2014-04-11 16:47:02 +0800

    增加 ListBoxUtil.fireChangeEvent()。 順便補另外兩個 method 的 JavaDoc。

--- src/dontCare/gf/gwt/client/util/ListBoxUtil.java
+++ src/dontCare/gf/gwt/client/util/ListBoxUtil.java
@@ -1,13 +1,35 @@
 package dontCare.gf.gwt.client.util;
 
+import javax.swing.event.ChangeEvent;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.DomEvent;
 import com.google.gwt.user.client.ui.ListBox;
 
 public class ListBoxUtil {
-	public static String selectedItem(ListBox lb) {
-		return lb.getItemText(lb.getSelectedIndex());
+	/**
+	 * @param listBox
+	 * @return 回傳 {@link ListBox#getSelectedIndex()} 對應的 item 值
+	 */
+	public static String selectedItem(ListBox listBox) {
+		return listBox.getItemText(listBox.getSelectedIndex());
 	}
 	
-	public static String selectedValue(ListBox lb) {
-		return lb.getValue(lb.getSelectedIndex());
+	/**
+	 * @param listBox
+	 * @return 回傳 {@link ListBox#getSelectedIndex()} 對應的 value 值
+	 */
+	public static String selectedValue(ListBox listBox) {
+		return listBox.getValue(listBox.getSelectedIndex());
+	}
+	
+	/**
+	 * 讓 ListBox 自動發出 {@link ChangeEvent}（觸發 {@link ChangeHandler}）。
+	 * {@link ListBox#getSelectedIndex()} 會是目前的值、沒有變動過。
+	 * @param listBox
+	 */
+	public static void fireChangeEvent(ListBox listBox) {
+		DomEvent.fireNativeEvent(Document.get().createChangeEvent(), listBox);
 	}
 }
